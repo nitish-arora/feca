524b61ad5b9531579ca90560eb691a079d0ccd8d
Nitish <nitish@nagarro.com>
2019-08-09 11:30:43 +0530

    Updates

---
+++ precache-manifest.7be832f123dd45c9ff86d7237320eba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa86644c898de89f439e0b2521cea414",
+    "url": "/feca/index.html"
+  },
+  {
+    "revision": "70abae747247a6742d1b",
+    "url": "/feca/static/css/main.7d9d8ee8.chunk.css"
+  },
+  {
+    "revision": "b7d7168d8ad1edd896d6",
+    "url": "/feca/static/js/2.5f8800c9.chunk.js"
+  },
+  {
+    "revision": "70abae747247a6742d1b",
+    "url": "/feca/static/js/main.2bef8efa.chunk.js"
+  },
+  {
+    "revision": "d6159b9bd82f48471cab",
+    "url": "/feca/static/js/runtime~main.2e851de5.js"
+  },
+  {
+    "revision": "bfd50cf9ffa7f94132df9685ec99f0d0",
+    "url": "/feca/static/media/hero-startpage-better-gaming-1920x1000.bfd50cf9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ebcc1cb7f5545505acc75bfc9847409.js"
+  "/feca/precache-manifest.7be832f123dd45c9ff86d7237320eba5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/feca/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
